a566ace64f66d297db37c51db81d34e847196ab8
Osam-Frimpong Schandorf <osamfrimpong@yahoo.com>
2023-05-12 18:29:23 +0000

    for push

--- app/Controllers/Http/LandingPagesController.ts
+++ app/Controllers/Http/LandingPagesController.ts
@@ -1,14 +1,13 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Programme from 'App/Models/Programme'
-import Student from 'App/Models/Student'
-import Tutor from 'App/Models/Tutor'
+import User from 'App/Models/User'
 
 export default class LandingPagesController {
   public async index({ view }: HttpContextContract) {
     const [programmes, tutors, students] = await Promise.all([
       Programme.all(),
-      Tutor.all(),
-      Student.all(),
+      User.query().where('user_type', 'tutor'),
+      User.query().where('user_type', 'student'),
     ])
 
     return view.render('home/index', {
